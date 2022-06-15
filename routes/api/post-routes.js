f7b4d484631bd7951f582f0aafd152f56be4c7dc
B-smd <djaja@iinet.net.au>
2022-06-15 22:07:42 +0800

    clean up

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -20,22 +20,6 @@ router.post("/", withAuth, (req, res) => {
 });
 
 router.put("/:id", withAuth, async (req, res) => {
-  // console.log(req.body, req.params.id)
-  // Post.update(req.body, {
-  //   where: {
-  //     id: req.params.id
-  //   }
-  // })
-  //   .then(affectedRows => {
-  //     if (affectedRows > 0) {
-  //       res.status(200).end();
-  //     } else {
-  //       res.status(404).end();
-  //     }
-  //   })
-  //   .catch(err => {
-  //     res.status(500).json(err);
-  //   });
   console.log(req.body);
   try {
     const [affectedRows] = await Post.update(req.body, {
